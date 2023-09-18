9e7ae59a879283d01ba4b63b6f5b66abce861fb7
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-09-18 21:27:18 +0530

    server activated

--- index.js
+++ index.js
@@ -12,9 +12,19 @@ const fileupload = require("express-fileupload")
 app.use(fileupload())
 
 //db connection
+const db = require("./config/database")
+db.connect()
 
 //cloud connection
+const cloudinary = require("./config/cloudinary")
+cloudinary.cloudinaryConnect()
 
 //api route mount 
+const Upload = require("./routes/FileUpload")
+app.use('api/v1/upload', Upload)
 
 //activate server
+app.listen(PORT, () => {
+    console.log(`app is running at ${PORT}`)
+})      
+
