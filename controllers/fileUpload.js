3e98e1c810e0998480b89df3552359a353268ac4
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-09-19 01:25:30 +0530

    file uploaded to cloudinary

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,5 +1,5 @@
 const File = require("../models/File")
-
+const cloudinary = require("cloudinary").v2
 //localfileupload -> handler function
 
 exports.localFileUpload =  async (req, res) => {
@@ -31,6 +31,11 @@ function isFileTypeSupported(type, supportedTypes){
     return supportedTypes.includes(type)
 }
 
+async function uploadFileToCloudinary(file, folder){
+    const options = {folder}
+    await cloudinary.uploader.upload(file.tempFilePath, options)
+}
+
 //image upload handler
 exports.imageUpload = async (req, res) => {
     try {
@@ -38,6 +43,7 @@ exports.imageUpload = async (req, res) => {
         const {name, tags, email} = req.body
         console.log(name, tags, email)
 
+        //file fetch
         const file = req.files.imageFile;
         console.log("imageUploadFile", file)
 
@@ -51,8 +57,27 @@ exports.imageUpload = async (req, res) => {
             message: "file format not supported"
           })  
         }
-        
+
+        const response = await uploadFileToCloudinary(file, "fileupload")
+
+        //entry in DB
+        // const fileData = await File.create({
+        //     name,
+        //     tags, 
+        //     email,
+        //     imageUrl
+        // })
+
+        res.json({
+            success: true,
+            message: "image successfully uploaded"
+        })
+
     } catch (error) {
-        
+            console.error(error)
+        res.status(400).json({
+            success: false,
+            message: "something went wrong"
+        })         
     }
 }
--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT
 //middleware add
 app.use(express.json())
 const fileupload = require("express-fileupload")
-app.use(fileupload())
+app.use(fileupload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/"
+}))
 
 //db connection
 const db = require("./config/database")
